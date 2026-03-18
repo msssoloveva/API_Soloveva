faa889ba5453b6eb49a897c230e30b843144bc2c
solovyovasa <msssoloveva26@gmail.com>
2026-03-18 12:37:07 +0300

    Soloveva_api_18/03 final!

--- test/test_delete_notes.py
+++ test/test_delete_notes.py
@@ -1,8 +1,8 @@
 class TestDeleteNotes:
     def test_delete_note(self, take_create_note, delete_notes):
-        note_id = take_create_note
-        response = delete_notes.delete_note(note_id)
+        assert take_create_note is not None
+        response = delete_notes.delete_note(take_create_note)
         assert response.status_code == 200
         json_response = response.json()
         assert json_response["message"] == "Note deleted!"
-        print(f"Заметка {note_id} успешно удалена")
+        print(f"Заметка {take_create_note} успешно удалена")
